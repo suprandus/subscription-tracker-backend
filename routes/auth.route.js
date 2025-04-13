4d8745d2fc1c494e875f93432ba585ce094e1675
Alberin Andus <130210722+suprandus@users.noreply.github.com>
2025-04-13 19:02:41 +0800

    fix: more http request samples

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,6 +2,11 @@ import { Router } from "express";
 
 const authRouter = Router();
 
+// GET
+authRouter.get("/", (req, res) =>
+  res.send({ title: "Welcome to auth routes" })
+);
+
 // POST
 authRouter.post("/sign-in", (req, res) => res.send({ title: "Sign In" }));
 authRouter.post("/sign-up", (req, res) => res.send({ title: "Sign Up" }));
--- routes/subscription.route.js
+++ routes/subscription.route.js
@@ -2,8 +2,39 @@ import { Router } from "express";
 
 const subscriptionRouter = Router();
 
+// GET
 subscriptionRouter.get("/", (req, res) =>
   res.send({ title: "GET all subscriptions" })
+);
+
+subscriptionRouter.get("/:id", (req, res) =>
+  res.send({ title: "GET subscription details" })
+);
+
+subscriptionRouter.get("/user/:id", (req, res) =>
+  res.send({ title: "GET all user subscriptions" })
+);
+
+subscriptionRouter.get("/upcoming-renewals", (req, res) =>
+  res.send({ title: "GET upcoming renewals" })
+);
+
+// POST
+subscriptionRouter.post("/", (req, res) =>
+  res.send({ title: "CREATE subscription" })
+);
+
+// PUT
+subscriptionRouter.put("/:id", (req, res) =>
+  res.send({ title: "UPDATE subscription" })
+);
+subscriptionRouter.put("/:id/cancel", (req, res) =>
+  res.send({ title: "CANCEL subscription" })
+);
+
+// DELETE
+subscriptionRouter.delete("/:id", (req, res) =>
+  res.send({ title: "DELETE subscription" })
 );
 
 export default subscriptionRouter;
