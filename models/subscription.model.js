619885703b4c1928e99ce66616555c2830d0ddc1
Alberin Andus <130210722+suprandus@users.noreply.github.com>
2025-04-13 19:04:24 +0800

    feat(models): add user and subscription scheme

---
+++ models/subscription.model.js
@@ -0,0 +1,92 @@
+import mongoose from "mongoose";
+
+const subscriptionSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Subscription name is required"],
+      trim: true,
+      minLength: 3,
+      maxLength: 100,
+    },
+    price: {
+      type: Number,
+      required: [true, "Subscription price is required"],
+      min: [0, "Must be greater than 0"],
+      max: [1000, "Must be less than 1000"],
+    },
+    currency: {
+      type: String,
+      enum: ["PHP", "USD", "EUR", "GBP"],
+      default: "PHP",
+    },
+    frequency: {
+      type: String,
+      enum: ["daily", "weekly", "monthly", "yearly"],
+    },
+    category: {
+      type: String,
+      enum: ["basic", "premium", "enterprise"],
+      default: "basic",
+      required: true,
+    },
+    paymentMethod: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    status: {
+      type: String,
+      enum: ["active", "cancelled", "expired"],
+      default: "active",
+    },
+    startDate: {
+      type: Date,
+      required: true,
+      validate: {
+        validator: (value) => value <= new Date(),
+        message: "Start date must be in the past",
+      },
+    },
+    renewalDate: {
+      type: Date,
+      required: true,
+      validate: {
+        validator: function (value) {
+          return value > this.startDate;
+        },
+      },
+      message: "Renewal date must be after start date",
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// Auto calculate if renewal date is missing
+subscriptionSchema.pre("save", function (next) {
+  if (!this.renewalDate) {
+    const renewalPeriods = {
+      daily: 1,
+      weekly: 7,
+      monthly: 30,
+      yearly: 365,
+    };
+
+    this.renewalDate = new Date(this.startDate);
+    this.renewalDate.setDate(
+      this.renewalDate.getDate() + renewalPeriods[this.frequency]
+    );
+  }
+
+  // Auto update the status if renewal has passed
+  if (this.renewalDate < new Date()) {
+    this.status = "expired";
+  }
+  next();
+});
